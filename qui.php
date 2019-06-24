b107ce3d42e4de9d573f01111dae681149ce0094
nelsonvan <nelson.vanicatte@lepoles.com>
2019-06-24 15:32:41 +0200

    page qui responsif

--- qui.php
+++ qui.php
@@ -1,2 +1,42 @@
 <?php require_once 'inc/header.inc.php' ?>
+
+<!-- Cartes de presentation  -->
+<div class="col-md-12">
+    <h2 class="title m-5">L'Hypnose pour qui ?</h2>
+</div>
+<!-- image 1 -->
+<div class="row">
+    <div class="col-sm-12 col-md-4">
+    <div class="card1" style="width: 18rem;">
+  <img src="img/freestock_129043757.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div> <!-- fin de card  -->
+</div><!-- Fin div row -->
+    </div>
+    <div class="col-sm-12 col-md-4">
+    <div class="card2" style="width: 18rem;">
+  <img src="img/freestock_115408432.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div><!-- Fin div image 2 -->
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <div class="card3" style="width: 18rem;">
+            <img src="img/freestock_93659512.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div><!-- Fin div image 3 -->
+</div>
+</div>
+
+
+
+
+
+
+
+
 <?php require_once 'inc/footer.inc.php' ?>
