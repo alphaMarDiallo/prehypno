ab70dba3cbb83a249b4259d4b4ba956440299510
nelsonvan <nelson.vanicatte@lepoles.com>
2019-06-24 15:39:07 +0200

    seance integration2.0

--- seance.php
+++ seance.php
@@ -1,2 +1,23 @@
 <?php require_once 'inc/header.inc.php' ?>
+<div class="row">
+<h3 class=" col-md-12 m-5">Dérouler d'une séance :</h3>
+<img src="img/cabinet.jpg" class="rounded-circle col-md-4 mt-5" style="height:30vh;">
+    <section class="sec1 col-md-8 mt-5">
+    <p>Nous commencerons par un entretien au cours duquel nous définirons ensemble votre ou vos
+        objectif et la maniére de les atteindredurablement. Je vous accompagne toujours avec
+        bienveillance et respect, sans jugement et de maniére personnalisée.
+    </p><br>
+    <p>S'ensuivant une ou plusieurs séances avec état modifié de conscience, vous ne risquerez absolument
+        rien et ne ferez rien que vous n'auriez pas envie de
+        faire, loin de perdre le controle, vous vous souviendrez de tout, vous parlerez et interagirez avec moi
+    </p><br>
+    <p>Le nombre de séances est variable mais il s'agit de thérapie bréve. En fonction des situations et des 
+        personnes, je pourrais étre amenée a vous proposer des 
+        << petits exercices >> a faire entre nos séances. 
+        </p><br>
+        <p>Une séance dure en moyenne 1h15, la premiére peut durer un peu plus longtemps.</p>
+    </section>
+    <p class="para offset-md-8 mt-5 ">
+       << Fais de ta vie un réve, une réalité >> Antoine de saint-Exupery 
+</div> 
 <?php require_once 'inc/footer.inc.php' ?>
