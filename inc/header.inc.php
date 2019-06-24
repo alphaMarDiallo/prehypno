17902a78cb5cfb7c6192e1b61389e2fe67636304
basim1979 <auguste.ba@lepoles.com>
2019-06-24 16:26:37 +0200

    Integration navBar + Debut NavToggle Jquery

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -15,17 +15,49 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Lien CSS personel -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-    <div class="container">
-        <header class="row">
-            <div class="col-md-2">logo</div>
-            <div class="col-md-2">23 rue de la folie Méricourt 75011</div>
-            <div class="col-md-2">2eme adresse</div>
-            <div class="col-md-2">Hypno-thérapeute humaniste</div>
-            <a href="#" class="col-md-2">Prise de RDV</a>
-            <div class="col-md-2">07 01 02 03 01</div>
-        </header><!-- Fin header -->
-        <main class="row">
+<div class="container">
+    <header class="row">
+        <nav class="navbar navbar-expand-lg navbar-light logo" id="nav1">
+  <!-- le logo signature -->
+            <img src="../img/logoFavIconAlpha.png " alt="" class="m-4"> 
+    <button class="navbar-toggler m-4" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item mt-4"> 
+          <p>23 rue de la folie Méricourt</p>
+          <p>75011 Paris</p>
+      </li>
+      <li class="nav-item m-4 mb-2">2eme adresse</li>
+      <li class="nav-item text text-center mt-4">
+         <p>Hypno-thérapeute</p>
+          <p>humaniste</p>
+      </li>
+      <li class="nav-item text text-center">
+        <a href="#" class="nav-link rounded-pill btn hover mt-3">Prendre un RDV</a>
+      <li class="nav-item m-4">
+        <a class="nav-link" href="#" >07 01 02 03 01</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<nav id="nav2">
+    <ul>
+      <li><a href="#">Qui</a></li>
+      <li><a href="#">Seance</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Temoignage</a></li>
+    </ul>
+    <a class="toggle-nav" href="#"></a>
+
+</nav>
+<main class="row">
+
+
+
+<?php require_once 'footer.inc.php'?>
